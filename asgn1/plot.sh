b00c81044fab284035cce76e530472b3cd6126a4
Kevin Lee <klee184>
2022-01-13 04:02:31 +0000

    added comments

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-#make clean && make sincos   # Rebuild the sincos executable.
-#./sincos > /tmp/sin.dat     # Place the data points into a file.
 
-rm -f /tmp/length.dat
-rm -f /tmp/max.dat
-rm -f /tmp/his.dat
+rm -f /tmp/length.dat                                                          #removes /tmp/length.dat
+rm -f /tmp/max.dat                                                             #removes /tmp/max.dat
+rm -f /tmp/his.dat                                                             #removes /tmp/his.dat
 
-make clean && make collatz
+make clean && make collatz                                                     #cleans program of files and makes collatz via MAKEFILE
 
-for n in {1..10000}; do
-	./collatz -n $n | wc -l >> /tmp/length.dat
-done
+#for loop to make a list of collatz sequence lengths
+for n in {1..10000}; do                                                        #for loop from 1 to 10000
+	./collatz -n $n | wc -l >> /tmp/length.dat                             #finds length of collatz sequence given length and appends to /tmp/length.dat
+done                                                                           #end of loop
 
-for n in {1..10000}; do
-	./collatz -n $n | sort -n | tail -n 1 >> /tmp/max.dat
-done
+#for loop to make a list of collatz sequence maximum values
+for n in {1..10000}; do                                                        #another for loop from 1 to 10000
+	./collatz -n $n | sort -n | tail -n 1 >> /tmp/max.dat                  #finds max value of collatz sequence given length and appends to /tmp/max.dat
+done                                                                           #end of loop
 
-head -n 10000 < /tmp/length.dat | sort | uniq -c | less > /tmp/his.dat
+#adding list of lengths and frequency
+head -n 10000 < /tmp/length.dat | sort | uniq -c | less > /tmp/his.dat         #uses sort, uniq, pipes, and head to put list of lengths and frequency into /tmp/his.dat
 
 
 # This is the heredoc that is sent to gnuplot.
 gnuplot <<END
+#gnuplot for collatz lengths is pretty simple
     set terminal pdf
     set output "length.pdf"
     set title "Collatz Sequence Lengths"
@@ -30,6 +32,7 @@ gnuplot <<END
     set zeroaxis
     plot "/tmp/length.dat" with dots title ""
 
+#gnuplot for max values, set yrange
     set terminal pdf
     set output "max.pdf"
     set title "Maximum Collatz Sequence Value"
@@ -39,6 +42,7 @@ gnuplot <<END
     set zeroaxis
     plot "/tmp/max.dat" with dots title ""
 
+#gnuplot for histogram, set xrange and yrange, flip x and y values when graphing
     set terminal pdf
     set output "his.pdf"
     set title "Collatz Sequence Length Histogram"
