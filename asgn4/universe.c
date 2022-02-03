1198090bc4e57a405e83f6229d0700eb58b46ee7
Kevin Lee <klee184>
2022-02-03 05:19:23 +0000

    added comments

--- asgn4/universe.c
+++ asgn4/universe.c
@@ -12,11 +12,14 @@ struct Universe {
 };
 
 Universe *uv_create(uint32_t rows, uint32_t cols, bool toroidal) {
+    //allocating memory for universe given size of rows and cols
     Universe *u = (Universe *) malloc(sizeof(Universe));
     u->grid = (bool **) calloc(rows, sizeof(bool *));
     for (uint32_t r = 0; r < rows; r += 1) {
         u->grid[r] = (bool *) calloc(cols, sizeof(bool));
     }
+    //declaring rows cols and toroidal
+    //return universe
     u->rows = rows;
     u->cols = cols;
     u->toroidal = toroidal;
@@ -24,6 +27,9 @@ Universe *uv_create(uint32_t rows, uint32_t cols, bool toroidal) {
 }
 
 void uv_delete(Universe *u) {
+    //free the memory alocated to the universe
+    //first need to free the contents of the grid
+    //then free the grid itself
     free(u->grid);
     free(u);
 }
@@ -37,17 +43,22 @@ uint32_t uv_cols(Universe *u) {
 }
 
 void uv_live_cell(Universe *u, uint32_t r, uint32_t c) {
+    //if the given coordinates are within the range of the rows and columns
+    //set the boolean at that coordinate to be true or alive
     if (r >= 0 && r < uv_rows(u) && c >= 0 && c < uv_cols(u)) {
         u->grid[r][c] = true;
     }
 }
 void uv_dead_cell(Universe *u, uint32_t r, uint32_t c) {
+    //same as uv_live_cell but set as false or dead
     if (r >= 0 && r < uv_rows(u) && c >= 0 && c < uv_cols(u)) {
         u->grid[r][c] = false;
     }
 }
 
 bool uv_get_cell(Universe *u, uint32_t r, uint32_t c) {
+    //if the given coordinates are within the range of the rows and columns
+    //get the boolean value from those coordinates
     if (r >= 0 && r < uv_rows(u) && c >= 0 && c < uv_cols(u)) {
         return u->grid[r][c];
     }
@@ -55,6 +66,7 @@ bool uv_get_cell(Universe *u, uint32_t r, uint32_t c) {
 }
 
 bool uv_populate(Universe *u, FILE *infile) {
+    //loop through the file and set the coordinates as alive using uv_live_cell
     for (uint32_t i = 0; i < uv_rows(u) * uv_cols(u); i += 1) {
         int r, c;
         fscanf(infile, "%d %d\n", &r, &c);
@@ -67,6 +79,8 @@ bool uv_populate(Universe *u, FILE *infile) {
 }
 
 uint32_t uv_census(Universe *u, uint32_t r, uint32_t c) {
+    //for toroidal set boudns for min and max for both rows and columns
+    //if the value at the given coordinate is true, increment num
     uint32_t num = 0;
     if (u->toroidal == true) {
         uint32_t r_min = (r + uv_rows(u) - 1) % uv_rows(u);
@@ -98,6 +112,9 @@ uint32_t uv_census(Universe *u, uint32_t r, uint32_t c) {
             num += 1;
         }
     } else {
+        //if not toroidal loop through one more and one less on both the rows and columns
+        //if the index is less than 0, or greater than the range don't pass through uv_get_cell
+        //otherwise, if the value there is true, increment num
         int r_min = r - 1;
         int r_max = r + 1;
         int c_min = c - 1;
@@ -120,6 +137,8 @@ uint32_t uv_census(Universe *u, uint32_t r, uint32_t c) {
 }
 
 void uv_print(Universe *u, FILE *outfile) {
+    //loop through the rows and columns, if the value at the coordinate is true
+    //print "o" otherwise, print ". "
     for (uint32_t row = 0; row < uv_rows(u); row += 1) {
         for (uint32_t col = 0; col < uv_cols(u); col += 1) {
             if (uv_get_cell(u, row, col) == true) {
